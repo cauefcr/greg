4547f5ee48945d4ac4ecdd357f60ef707e54d33d
Cauê Felchar <caue.fcr@gmail.com>
2021-04-23 06:30:57 -0300

    oops

--- portscan_test.go
+++ portscan_test.go
@@ -1,10 +1,7 @@
 package greg_test
 
 import (
-	"fmt"
 	"net"
-	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/cauefcr/greg"
@@ -12,7 +9,6 @@ import (
 
 func TestScanner(T *testing.T) {
 	ports := []string{}
-	i := 0
 	go func() {
 		listener, err := net.Listen("tcp", "127.0.0.1:8666")
 		if err != nil {
@@ -25,14 +21,9 @@ func TestScanner(T *testing.T) {
 		conn.Write([]byte("the game"))
 		conn.Close()
 	}()
-	ports = append(ports, fmt.Sprintf("800%v", i))
 	for addr := range greg.PortScan([]string{"127.0.0.1"}, ports) {
-		portEnd, err := strconv.ParseInt(strings.Split(addr, "8666")[1], 10, 32)
-		if err != nil {
-			T.Error(err)
-		}
-		if portEnd > 0 && portEnd < 100 {
-			T.Errorf("listener closed")
+		if addr != "127.0.0.1:8666" {
+			T.Errorf("failed to scan open port")
 		}
 	}
 }
